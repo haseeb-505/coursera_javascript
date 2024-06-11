1592eeb9602aceac027d39dd11d1a84c7822d595
Haseeb Ahmad Tahir <hatahir87626@gmail.com>
2024-06-11 16:58:25 +0500

    out of block scope of varibale types is successfully explained

--- variable_scope_practice/scope_lab.js
+++ variable_scope_practice/scope_lab.js
@@ -9,16 +9,21 @@ const globalConst = "I'm a global constant.";
     var blockVar = "I'm a block-scope var";
     let blockLet = "I'm a block-scope let";
     const blockConst = "I'm a block-scope const.";
+    // blockVar = "Hello How are you"; // reassingment of var was successful.
+
+    // blockLet = "Block let reassigned"
+    
 }
 // Global scope
 console.log(globalVar); // Output: "I'm a global variable"
 console.log(globalLet); // Output: "I'm also global, but scoped with let"
 console.log(globalConst); // Output: "I'm a global constant"
 
-console.log("\nNow, we'll call block level messages to print.\n")
+console.log("\nNow, we'll call block level messages to print.\n");
 
 //Block Scope
-// console.log(blockVar);
+blockVar = "Out of block reassignment. However, blockLet is not accessible out of block."; // out of block reassignment was successful
+console.log(blockVar);
 // console.log(blockLet);
 
 // function scope
